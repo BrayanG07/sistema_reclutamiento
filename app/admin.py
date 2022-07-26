213640df0068d81a3c708d3208fd424294da09b7
BrayanUTH <10brayanalvarez@gmail.com>
2022-07-26 15:24:01 -0600

    Super usuario creado y registrado los modelos

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# Register your models here.
+admin.site.register(Job_Position)
+admin.site.register(Vacant)
+admin.site.register(Skills)
